18febc2fb19870a13002f32b43a7356f19c74abb
Joao Brito <joaojr.89@gmail.com>
2017-09-07 16:32:50 -0300

    add set parameters

--- optimizer/optimizer.go
+++ optimizer/optimizer.go
@@ -9,14 +9,26 @@ import (
 // Optimizer defines a structure optimizer algorithm
 type Optimizer interface {
 	Search(numSolutions, timeAvailable int) interface{}
+	SetDefaultParameters()
+	SetFileParameters(parms map[string]string)
 }
 
 // Create creates a structure optimizer algorithm
 func Create(optimizerAlg string, scoreRankers []score.Ranker, parmFile string) Optimizer {
 	switch optimizerAlg {
 	case "sample":
-		return NewSampleSearch(scoreRankers, parmFile)
+		return newSampleSearch(scoreRankers, parmFile)
 	default:
 		panic(fmt.Errorf("invalid algorithm option: '%v'", optimizerAlg))
 	}
+}
+
+func setParameters(alg Optimizer, parmFile string) {
+	alg.SetDefaultParameters()
+	parms := readParametersFile(parmFile)
+	alg.SetFileParameters(parms)
+}
+
+func readParametersFile(parmFile string) map[string]string {
+	return nil
 }
--- optimizer/sample.go
+++ optimizer/sample.go
@@ -1,17 +1,36 @@
 package optimizer
 
-import "github.com/britojr/btbn/score"
+import (
+	"github.com/britojr/btbn/score"
+	"github.com/britojr/utl/conv"
+)
 
 // SampleSearch implements the sampling strategy
-type SampleSearch struct {
+type sampleSearch struct {
+	tw int //treewidth
 }
 
 // NewSampleSearch creates a instance of the sample stragegy
-func NewSampleSearch(scoreRankers []score.Ranker, parmFile string) *SampleSearch {
-	return nil
+func newSampleSearch(scoreRankers []score.Ranker, parmFile string) Optimizer {
+	s := new(sampleSearch)
+	setParameters(s, parmFile)
+	return s
 }
 
 // Search search network structure
-func (s *SampleSearch) Search(numSolutions, timeAvailable int) interface{} {
+func (s *sampleSearch) Search(numSolutions, timeAvailable int) interface{} {
 	return nil
+}
+
+// SetDefaultParameters sets the defaults
+func (s *sampleSearch) SetDefaultParameters() {
+	// set internal variables to defined constants
+	s.tw = 3
+}
+
+// SetFileParameters sets parameters from input file
+func (s *sampleSearch) SetFileParameters(parms map[string]string) {
+	if tw, ok := parms["treewidth"]; ok {
+		s.tw = conv.Atoi(tw)
+	}
 }
