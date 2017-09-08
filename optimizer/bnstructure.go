c792608b9b462d1c7a0eb3faaa3a1f20b4c5b925
Joao Brito <joaojr.89@gmail.com>
2017-09-08 19:33:47 -0300

    add bnstructure methods

--- optimizer/bnstructure.go
+++ optimizer/bnstructure.go
@@ -1,5 +1,7 @@
 package optimizer
 
+import "github.com/britojr/btbn/varset"
+
 // BNStructure defines a structure solution
 type BNStructure struct {
 	scoreVal float64
@@ -18,4 +20,14 @@ func (b *BNStructure) Better(other *BNStructure) bool {
 // Score returns the structure score
 func (b *BNStructure) Score() float64 {
 	return b.scoreVal
+}
+
+// SetParents assigns the parent set for a variable and its respective local score
+func (b *BNStructure) SetParents(v int, parents varset.Varset, localScore float64) {
+	panic("not implemented")
+}
+
+// LocalScore returns the local score of a family
+func (b *BNStructure) LocalScore(v int) float64 {
+	panic("not implemented")
 }
