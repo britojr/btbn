1c48d9aa97cf4744bc95681187fec7cf97b4485f
Joao Brito <joaojr.89@gmail.com>
2017-09-11 16:16:32 -0300

    export sample type

--- optimizer/sample.go
+++ optimizer/sample.go
@@ -9,15 +9,15 @@ import (
 )
 
 // SampleSearch implements the sampling strategy
-type sampleSearch struct {
+type SampleSearch struct {
 	tw           int            //treewidth
 	nv           int            // number of variables
 	scoreRankers []score.Ranker // score rankers for each variable
 }
 
 // NewSampleSearch creates a instance of the sample stragegy
-func newSampleSearch(scoreRankers []score.Ranker, parmFile string) Optimizer {
-	s := new(sampleSearch)
+func newSampleSearch(scoreRankers []score.Ranker, parmFile string) *SampleSearch {
+	s := new(SampleSearch)
 	s.scoreRankers = scoreRankers
 	s.nv = len(s.scoreRankers)
 	setParameters(s, parmFile)
@@ -26,27 +26,27 @@ func newSampleSearch(scoreRankers []score.Ranker, parmFile string) Optimizer {
 }
 
 // Search searchs for a network structure
-func (s *sampleSearch) Search() *BNStructure {
+func (s *SampleSearch) Search() *BNStructure {
 	tk := ktree.UniformSample(s.nv, s.tw)
 	bn := DAGapproximatedLearning(tk, s.scoreRankers)
 	return bn
 }
 
 // SetDefaultParameters sets the defaults
-func (s *sampleSearch) SetDefaultParameters() {
+func (s *SampleSearch) SetDefaultParameters() {
 	// set internal variables to defined constants
 	s.tw = 3
 }
 
 // SetFileParameters sets parameters from input file
-func (s *sampleSearch) SetFileParameters(parms map[string]string) {
+func (s *SampleSearch) SetFileParameters(parms map[string]string) {
 	if tw, ok := parms["treewidth"]; ok {
 		s.tw = conv.Atoi(tw)
 	}
 }
 
 // Validate validates parameters
-func (s *sampleSearch) validate() {
+func (s *SampleSearch) validate() {
 	if s.tw <= 0 || s.nv < s.tw+2 {
 		log.Printf("n=%v, tw=%v\n", s.nv, s.tw)
 		log.Panic("Invalid treewidth! Choose values such that: n >= tw+2 and tw > 0")
