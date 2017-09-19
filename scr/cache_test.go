0a9f05c4eaabbce39ecd49c9259584c683c63067
Joao Brito <joaojr.89@gmail.com>
2017-09-19 17:05:19 -0300

    scr: use temp file for tests

--- scr/cache_test.go
+++ scr/cache_test.go
@@ -1,46 +1,68 @@
 package scr
 
 import (
+	"os"
 	"testing"
 
 	"github.com/britojr/btbn/varset"
 )
 
 func TestScores(t *testing.T) {
+	content := `META pss_version = 0.1
+META teste 0,1,2,3,4
+META num_variables=5
+
+VAR A
+-2
+-9 B
+-8 C
+-6 B C
+
+VAR B
+-9
+-9 A
+-1 C
+-3 A C
+
+VAR C
+-10
+-10.1 A
+-2 B
+-1 A B
+`
 	cases := []struct {
-		scoreFile string
-		x         int
-		pxScores  []struct {
+		content  string
+		x        int
+		pxScores []struct {
 			vars []int
 			scor float64
 		}
-	}{
-		{"scorefile.test", 0,
-			[]struct {
-				vars []int
-				scor float64
-			}{
-				{[]int{}, -2},
-				{[]int{1}, -9},
-				{[]int{2}, -8},
-				{[]int{1, 2}, -6},
-			},
-		}, {
-			"scorefile.test", 2,
-			[]struct {
-				vars []int
-				scor float64
-			}{
-				{[]int{}, -10},
-				{[]int{0}, -10.1},
-				{[]int{1}, -2},
-				{[]int{0, 1}, -1},
-			},
-		},
-	}
+	}{{
+		content, 0,
+		[]struct {
+			vars []int
+			scor float64
+		}{
+			{[]int{}, -2},
+			{[]int{1}, -9},
+			{[]int{2}, -8},
+			{[]int{1, 2}, -6},
+		}}, {
+		content, 2,
+		[]struct {
+			vars []int
+			scor float64
+		}{
+			{[]int{}, -10},
+			{[]int{0}, -10.1},
+			{[]int{1}, -2},
+			{[]int{0, 1}, -1},
+		}}}
 
 	for _, tt := range cases {
-		sc := Read(tt.scoreFile)
+		fname := helperGetTempFile(tt.content, "rk_test")
+		defer os.Remove(fname)
+		sc := Read(fname)
 		scoreMap := sc.Scores(tt.x)
 		for _, px := range tt.pxScores {
 			parents := varset.New(sc.Nvar())
--- scr/ranker_test.go
+++ scr/ranker_test.go
@@ -1,6 +1,7 @@
 package scr
 
 import (
+	"os"
 	"reflect"
 	"testing"
 
@@ -16,26 +17,49 @@ func TestScoreOf(t *testing.T) {
 		p[s[i]] = varset.New(len(s[i]))
 		p[s[i]].LoadHashString(s[i])
 	}
+	content := `META pss_version = 0.1
+META teste 0,1,2,3,4
+META num_variables=5
 
+VAR A
+-2
+-9 B
+-8 C
+-6 B C
+
+VAR B
+-9
+-9 A
+-1 C
+-3 A C
+
+VAR C
+-10
+-10.1 A
+-2 B
+-1 A B
+`
 	cases := []struct {
-		scoreFile string
-		maxPa, x  int
-		vars      []int
-		scor      float64
+		content  string
+		maxPa, x int
+		vars     []int
+		scor     float64
 	}{
-		{"scorefile.test", 0, 0, []int{}, -2},
-		{"scorefile.test", 0, 0, []int{1}, -9},
-		{"scorefile.test", 0, 0, []int{2}, -8},
-		{"scorefile.test", 0, 0, []int{1, 2}, -6},
+		{content, 0, 0, []int{}, -2},
+		{content, 0, 0, []int{1}, -9},
+		{content, 0, 0, []int{2}, -8},
+		{content, 0, 0, []int{1, 2}, -6},
 
-		{"scorefile.test", 0, 2, []int{}, -10},
-		{"scorefile.test", 0, 2, []int{0}, -10.1},
-		{"scorefile.test", 0, 2, []int{1}, -2},
-		{"scorefile.test", 0, 2, []int{0, 1}, -1},
+		{content, 0, 2, []int{}, -10},
+		{content, 0, 2, []int{0}, -10.1},
+		{content, 0, 2, []int{1}, -2},
+		{content, 0, 2, []int{0, 1}, -1},
 	}
 
 	for _, tt := range cases {
-		sr := CreateRankers(Read(tt.scoreFile), tt.maxPa)
+		fname := helperGetTempFile(tt.content, "rk_test")
+		defer os.Remove(fname)
+		sr := CreateRankers(Read(fname), tt.maxPa)
 		parents := varset.New(len(sr))
 		for _, v := range tt.vars {
 			parents.Set(v)
@@ -48,23 +72,47 @@ func TestScoreOf(t *testing.T) {
 }
 
 func TestBestIn(t *testing.T) {
+	content := `META pss_version = 0.1
+META teste 0,1,2,3,4
+META num_variables=5
+
+VAR A
+-2
+-9 B
+-8 C
+-6 B C
+
+VAR B
+-9
+-9 A
+-1 C
+-3 A C
+
+VAR C
+-10
+-10.1 A
+-2 B
+-1 A B
+`
 	cases := []struct {
-		scoreFile string
-		maxPa, x  int
-		restric   []int
-		wantPa    []int
-		wantSc    float64
+		content  string
+		maxPa, x int
+		restric  []int
+		wantPa   []int
+		wantSc   float64
 	}{
-		{"scorefile.test", 0, 0, []int{0, 1, 2}, []int{}, -2},
-		{"scorefile.test", 0, 0, []int{}, []int{}, -2},
-		{"scorefile.test", 0, 1, []int{0, 1, 2}, []int{2}, -1},
-		{"scorefile.test", 0, 2, []int{0, 1, 2}, []int{0, 1}, -1},
-		{"scorefile.test", 0, 2, []int{1, 2}, []int{1}, -2},
-		{"scorefile.test", 0, 2, []int{0, 2}, []int{}, -10},
+		{content, 0, 0, []int{0, 1, 2}, []int{}, -2},
+		{content, 0, 0, []int{}, []int{}, -2},
+		{content, 0, 1, []int{0, 1, 2}, []int{2}, -1},
+		{content, 0, 2, []int{0, 1, 2}, []int{0, 1}, -1},
+		{content, 0, 2, []int{1, 2}, []int{1}, -2},
+		{content, 0, 2, []int{0, 2}, []int{}, -10},
 	}
 
 	for _, tt := range cases {
-		sr := CreateRankers(Read(tt.scoreFile), tt.maxPa)
+		fname := helperGetTempFile(tt.content, "rk_test")
+		defer os.Remove(fname)
+		sr := CreateRankers(Read(fname), tt.maxPa)
 		restric := varset.New(len(sr))
 		for _, v := range tt.restric {
 			restric.Set(v)
