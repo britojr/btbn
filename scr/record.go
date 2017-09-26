ca163f8007d0b31f6720e2cbdd22c0cd6576076b
Joao Brito <joaojr.89@gmail.com>
2017-09-26 20:45:18 -0300

    scr: add heap interface to record

--- scr/record.go
+++ scr/record.go
@@ -29,3 +29,20 @@ func (r *Record) Data() interface{} {
 func (r *Record) Score() float64 {
 	return r.score
 }
+
+// RecordSlice defines a slice of records that implement heap interface
+type RecordSlice []*Record
+
+func (rs RecordSlice) Len() int           { return len(rs) }
+func (rs RecordSlice) Less(i, j int) bool { return rs[i].Score() > rs[j].Score() }
+func (rs RecordSlice) Swap(i, j int)      { rs[i], rs[j] = rs[j], rs[i] }
+
+// Push appends a record to record slice
+func (rs *RecordSlice) Push(x interface{}) { *rs = append(*rs, x.(*Record)) }
+
+// Pop removes the highest scoring record from record slice
+func (rs *RecordSlice) Pop() interface{} {
+	rec := (*rs)[len(*rs)-1]
+	*rs = (*rs)[:len(*rs)-1]
+	return rec
+}
