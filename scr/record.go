ed58c44610ee01127db5dc8db7515b518f433e35
Joao Brito <joaojr.89@gmail.com>
2017-09-27 13:49:04 -0300

    scr: add heap interface

--- scr/record.go
+++ scr/record.go
@@ -32,19 +32,28 @@ func (r *Record) Score() float64 {
 	return r.score
 }
 
-// RecordSlice defines a slice of records that implement heap interface
-type RecordSlice []*Record
+// RecordHeap defines a heap implementation for a slice of records
+type RecordHeap struct {
+	recs *[]*Record
+	less func(i, j int) bool
+}
 
-func (rs RecordSlice) Len() int           { return len(rs) }
-func (rs RecordSlice) Less(i, j int) bool { return rs[i].Score() < rs[j].Score() }
-func (rs RecordSlice) Swap(i, j int)      { rs[i], rs[j] = rs[j], rs[i] }
+// NewRecordHeap creates a new record (min or max) heap
+func NewRecordHeap(recs *[]*Record, less func(i, j int) bool) *RecordHeap {
+	return &RecordHeap{recs, less}
+}
 
-// Push appends a record to record slice
-func (rs *RecordSlice) Push(x interface{}) { *rs = append(*rs, x.(*Record)) }
+func (rh RecordHeap) Len() int           { return len(*(rh.recs)) }
+func (rh RecordHeap) Swap(i, j int)      { (*rh.recs)[i], (*rh.recs)[j] = (*rh.recs)[j], (*rh.recs)[i] }
+func (rh RecordHeap) Less(i, j int) bool { return rh.less(i, j) }
 
-// Pop removes the highest scoring record from record slice
-func (rs *RecordSlice) Pop() interface{} {
-	rec := (*rs)[len(*rs)-1]
-	*rs = (*rs)[:len(*rs)-1]
+// Push appends a record to record heap
+func (rh *RecordHeap) Push(x interface{}) { *rh.recs = append(*rh.recs, x.(*Record)) }
+
+// Pop removes the highest scoring record from record heap
+func (rh *RecordHeap) Pop() interface{} {
+	n := len(*rh.recs) - 1
+	rec := (*rh.recs)[n]
+	*rh.recs = (*rh.recs)[:n]
 	return rec
 }
---
+++ scr/record_test.go
@@ -0,0 +1,59 @@
+package scr
+
+import (
+	"container/heap"
+	"reflect"
+	"testing"
+)
+
+func TestRecordHeap(t *testing.T) {
+	rs1 := []*Record{&Record{5, nil}, &Record{2, nil}, &Record{7, nil}}
+	rs2 := []*Record{&Record{5, nil}, &Record{2, nil}, &Record{7, nil}}
+	cases := []struct {
+		init    *[]*Record
+		less    func(i, j int) bool
+		actions []func(*RecordHeap)
+		want    []float64
+	}{{
+		init: &rs1,
+		less: func(i, j int) bool { return rs1[i].score < rs1[j].score },
+		actions: []func(*RecordHeap){
+			func(h *RecordHeap) { heap.Push(h, NewRecord(9, nil)) },
+			func(h *RecordHeap) { heap.Push(h, NewRecord(3, nil)) },
+			func(h *RecordHeap) { heap.Pop(h) },
+			func(h *RecordHeap) { heap.Push(h, NewRecord(1, nil)) },
+			func(h *RecordHeap) { heap.Pop(h) },
+		},
+		want: []float64{3, 5, 7, 9},
+	}, {
+		init: &rs2,
+		less: func(i, j int) bool { return rs2[i].score > rs2[j].score },
+		actions: []func(*RecordHeap){
+			func(h *RecordHeap) { heap.Push(h, NewRecord(9, nil)) },
+			func(h *RecordHeap) { heap.Push(h, NewRecord(3, nil)) },
+			func(h *RecordHeap) { heap.Pop(h) },
+			func(h *RecordHeap) { heap.Push(h, NewRecord(1, nil)) },
+			func(h *RecordHeap) { heap.Pop(h) },
+		},
+		want: []float64{5, 3, 2, 1},
+	}}
+
+	for _, tt := range cases {
+		h := NewRecordHeap(tt.init, tt.less)
+		heap.Init(h)
+		for _, run := range tt.actions {
+			run(h)
+		}
+		got := helperGetPopKeys(h)
+		if !reflect.DeepEqual(tt.want, got) {
+			t.Errorf("heap not working (%v) != (%v)", tt.want, got)
+		}
+	}
+}
+
+func helperGetPopKeys(h *RecordHeap) (ks []float64) {
+	for h.Len() > 0 {
+		ks = append(ks, heap.Pop(h).(*Record).Score())
+	}
+	return
+}
